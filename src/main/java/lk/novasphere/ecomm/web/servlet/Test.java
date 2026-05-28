98b3e19dc07a6cd179b666afd910cdc1da06ab0c
Maleesha Hirushan <maleeshahir@gmail.com>
2026-05-28 20:28:10 +0530

    Day 15 | End

--- src/main/java/lk/novasphere/ecomm/web/servlet/Test.java
+++ src/main/java/lk/novasphere/ecomm/web/servlet/Test.java
@@ -5,13 +5,31 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.novasphere.ecomm.user.dto.UserDTO;
+import lk.novasphere.ecomm.user.remote.UserRemote;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/test")
 public class Test extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().write("E-comm Web module test.");
+
+        try {
+            InitialContext ic = new InitialContext();
+            UserRemote userRemote = (UserRemote) ic.lookup("java:global/ecomm-user-1.0/UserSessionBean");
+
+            List<UserDTO> allUsers = userRemote.getAllUsers();
+            for (UserDTO user : allUsers) {
+                user.toString();
+            }
+
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
